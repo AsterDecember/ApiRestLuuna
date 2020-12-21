d5b0ef445047adec9bfc16baa791dbf595b88580
AsterHavok <aster.december@gmail.com>
2020-12-20 19:49:28 -0600

    [ADDED] jwtRoutes added on products protected routes as middleware

--- routes/products.js
+++ routes/products.js
@@ -1,53 +1,54 @@
-let express = require('express');
-let router = express.Router();
+let express = require('express')
+let router = express.Router()
 const Product = require('../models/Product')
-const {getDbObject} = require('../middleware/getDbObject.js');
+const {getDbObject} = require('../middleware/getDbObject.js')
+const {jwtRoutes} = require('../middleware/protectedRoutes.js')
 
 router.get('/', async (req, res) => {
     try {
         const products = await Product.find()
-        res.json(products)
+        res.status(200).json(products)
     } catch (err) {
         res.status(500).json({message: err.message})
     }
-});
+})
 
-router.post("/", async (req, res) => {
+router.post("/",jwtRoutes, async (req, res) => {
     const product = new Product({
         sku: req.body.sku,
         name: req.body.name,
         price: req.body.price,
         brand: req.body.brand,
-    });
+    })
     try {
-        const newProduct = await product.save();
-        res.status(201).json({ newProduct });
+        const newProduct = await product.save()
+        res.status(201).json({ newProduct })
     } catch (err) {
-        res.status(400).json({ message: err.message });
+        res.status(400).json({ message: err.message })
     }
-});
+})
 
 router.get("/:id", getDbObject(Product), (req, res) => {
-    res.json(res.dbObject);
-});
+    res.status(200).json(res.dbObject)
+})
 
-router.put("/:id", getDbObject(Product), async (req, res) => {
+router.put("/:id",jwtRoutes, getDbObject(Product), async (req, res) => {
     try {
         const updatedProduct = await res.dbObject.set(req.body)
-        const dbUpdatedProduct = await updatedProduct.save();
-        res.json(dbUpdatedProduct);
+        const dbUpdatedProduct = await updatedProduct.save()
+        res.status(200).json(dbUpdatedProduct)
     } catch (err) {
-        res.status(400).json({ message: err.message });
+        res.status(400).json({ message: err.message })
     }
-});
+})
 
-router.delete("/:id", getDbObject(Product), async (req, res) => {
+router.delete("/:id",jwtRoutes, getDbObject(Product), async (req, res) => {
     try {
-        await res.dbObject.deleteOne();
-        res.json({ message: "Product has been deleted" });
+        await res.dbObject.deleteOne()
+        res.json({ message: "Product has been deleted" })
     } catch (err) {
-        res.status(400).json({ message: err.message });
+        res.status(400).json({ message: err.message })
     }
-});
+})
 
-module.exports = router;
+module.exports = router
